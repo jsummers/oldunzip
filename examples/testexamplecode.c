65fe3b5b19eff700c171d2409d8b70c18bfb5346
Jason Summers <jason1@pobox.com>
2019-11-04 10:22:05 -0500

    Changed the test files for implode and reduce

--- examples/testexamplecode.c
+++ examples/testexamplecode.c
@@ -44,8 +44,8 @@ static int test_ozunreduce(void)
 {
 	FILE *infile, *outfile;
 	const char *infn = "example-r.zip";
-	long cmpr_offset = 37;
-	OZUR_OFF_T cmpr_size = 268;
+	long cmpr_offset = 35;
+	OZUR_OFF_T cmpr_size = 276;
 	unsigned int cmpr_factor = 4;
 	const char *outfn = "example-r.out";
 	OZUR_OFF_T uncmpr_size = 2048;
@@ -72,11 +72,11 @@ static int test_unimplode6a(void)
 {
 	FILE *infile, *outfile;
 	const char *infn = "example-i.zip";
-	long cmpr_offset = 37;
-	UI6A_OFF_T cmpr_size = 28;
+	long cmpr_offset = 35;
+	UI6A_OFF_T cmpr_size = 89;
 	UI6A_UINT16 bit_flags = UI6A_FLAG_4KDICT | UI6A_FLAG_2TREES;
 	const char *outfn = "example-i.out";
-	UI6A_OFF_T uncmpr_size = 29;
+	UI6A_OFF_T uncmpr_size = 2048;
 
 	printf("[unimplode6a]\n");
 	infile = fopen(infn, "rb");
